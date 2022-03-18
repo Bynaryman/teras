aef13851aba892259ad1b4c82278b5363ca70b8c
zerotoasic <zerotoasic@zerotoasic.vm.com>
2022-03-18 17:30:48 +0100

    update bridge

--- src/teras_bridge_mpw5.v
+++ src/teras_bridge_mpw5.v
@@ -5,10 +5,6 @@
  */
 
 module teras_bridge_mpw5 (
-`ifdef USE_POWER_PINS
-    inout vccd1,	// User area 1 1.8V supply
-    inout vssd1,	// User area 1 digital ground
-`endif
 
     // Wishbone Slave ports (WB MI A)
     input wb_clk_i,
@@ -22,18 +18,11 @@ module teras_bridge_mpw5 (
     output wbs_ack_o,
     output [31:0] wbs_dat_o,
 
-    // Logic Analyzer Signals
-    input  [127:0] la_data_in,
-    output [127:0] la_data_out,
-    input  [127:0] la_oenb,
-
     // IOs
     input  [`MPRJ_IO_PADS-1:0] io_in,
     output [`MPRJ_IO_PADS-1:0] io_out,
     output [`MPRJ_IO_PADS-1:0] io_oeb,
 
-    // IRQ
-    output [2:0] irq
 );
     wire clk;
     wire rst;
@@ -55,14 +44,8 @@ module teras_bridge_mpw5 (
     assign io_out[32]   = matrix_c_valid;
     assign io_oeb = {(`MPRJ_IO_PADS-1){rst}};
 
-    // IRQ
-    assign irq = 3'b000;	// Unused
-
-    // LA
-    assign la_data_out = {{(127-33){1'b0}}, matrix_c_valid, matrix_c_out};
-    // Assuming LA probes [65:64] are for controlling the count clk & reset  
-    assign clk = (~la_oenb[64]) ? la_data_in[64]: wb_clk_i;
-    assign rst = (~la_oenb[65]) ? la_data_in[65]: wb_rst_i;
+    assign clk = wb_clk_i;
+    assign rst = wb_rst_i;
 
     teras teras_inst (
         // System signals
